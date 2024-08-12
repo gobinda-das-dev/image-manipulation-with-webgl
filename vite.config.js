60bf8a60410c74f4524356225c3355a1184ce7a0
Gobinda Das <gobinda993720@gmail.com>
2024-08-12 19:22:10 +0530

    update vite config for multiple routes

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,15 @@ import glsl from 'vite-plugin-glsl';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: 'index.html', // Main entry point
+        project1: '01/index.html', // Route for 01 project
+        project2: '02/index.html', // Route for 02 project
+        project3: '03/index.html', // Route for 03 project
+      },
+    },
+  },
   plugins: [glsl()]
 });
