7a83635b209eb439fccffd871d70bcf30f151fbc
gobinda das <gobinda993720@gmail.com>
2024-09-29 12:27:02 +0530

    update code

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,20 @@
 import glsl from 'vite-plugin-glsl';
 import { defineConfig } from 'vite';
 
+const noOfPages = 8;
+const inputs = {};
+
+for (let i = 1; i <= noOfPages; i++) {
+  inputs[`project${i}`] = `./${i.toString().padStart(2, '0')}/index.html`;
+}
+
+
 export default defineConfig({
   build: {
     rollupOptions: {
       input: {
         main: 'index.html', // Main entry point
-        project1: '01/index.html', // Route for 01 project
-        project2: '02/index.html', // Route for 02 project
-        project3: '03/index.html', // Route for 03 project
+        ...inputs
       },
     },
   },
