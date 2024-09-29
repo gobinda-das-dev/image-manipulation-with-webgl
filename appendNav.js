b5623945f7353d8a3e42853d073ae040037fcb19
gobinda das <gobinda993720@gmail.com>
2024-09-29 11:58:02 +0530

    update cursor

--- appendNav.js
+++ appendNav.js
@@ -52,30 +52,67 @@ function appendCursor() {
 }
 
 function handleCursorMovement() {
+   const cursor = $('#cursor');
+
+   const mouse = { x: 0, y: 0 }
+   const smoothMouse = { x: 0, y: 0 }
+   const mouseVelocity = { x: 0, y: 0 }
+   const lerp = (x, y, a) => x * (1 - a) + y * a;
+
+   window.addEventListener('mousemove', (e) => {
+      mouse.x = e.clientX;
+      mouse.y = e.clientY;
+   })
+
+   gsap.set(cursor, { xPercent: -50, yPercent: -50 })
+   const setter = {
+      x: gsap.quickSetter(cursor, 'x', 'px'),
+      y: gsap.quickSetter(cursor, 'y', 'px'),
+      scaleY: gsap.quickSetter(cursor, 'scaleY'),
+      scaleX: gsap.quickSetter(cursor, 'scaleX'),
+      rotation: gsap.quickSetter(cursor, 'rotation', 'deg'),
+      wc: gsap.quickSetter(cursor, 'willChange'),
+   }
+
+   gsap.ticker.add(() => {
+      smoothMouse.x = lerp(smoothMouse.x, mouse.x, 0.15);
+      smoothMouse.y = lerp(smoothMouse.y, mouse.y, 0.15);
+
+      mouseVelocity.x = Math.abs(mouse.x - smoothMouse.x);
+      mouseVelocity.y = Math.abs(mouse.y - smoothMouse.y);
+
+      const angle = Math.atan2(mouse.y - smoothMouse.y, mouse.x - smoothMouse.x) * (180 / Math.PI);
+      const scaleAmount = Math.min((mouseVelocity.x + mouseVelocity.y) * 0.0035, 0.5);
+
+      setter.x(smoothMouse.x);
+      setter.y(smoothMouse.y);
+      setter.scaleY(1 - scaleAmount);
+      setter.scaleX(1 + scaleAmount);
+      setter.rotation(angle);
+      setter.wc('transform');
+   })
+
+
+
+
+
+
+
+
+
+
+
+
+
    // handle cursor movement
-   document.body.addEventListener('mousemove', ({ x, y }) => {
-      gsap.to('#cursor', {
-         x, y,
-         ease: 'expo',
-         duration: 0.7
-      });
-   });
-   document.body.addEventListener('touchmove', (e) => {
-      const touch = e.touches[0];
-      gsap.to('#cursor', {
-         x: touch.clientX,
-         y: touch.clientY,
-         ease: 'expo',
-         duration: 0.7
-      });
-   });
    const cursorTw = gsap.to('#cursor', {
       height: 20,
       width: 20,
       ease: 'power1.inOut',
-      paused: true
+      paused: true,
+      duration: 0.3
    });
-   
+
    const image = $('.images img');
    image.addEventListener('mouseenter', () => cursorTw.play());
    image.addEventListener('touchstart', () => cursorTw.play());
