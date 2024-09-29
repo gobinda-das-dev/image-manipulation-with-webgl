467dbe5aeefa9ac511016f21d979e72bccb501ff
gobinda das <gobinda993720@gmail.com>
2024-09-29 13:42:53 +0530

    change 03 effect

--- 03/shaders/fragment.glsl
+++ 03/shaders/fragment.glsl
@@ -12,12 +12,9 @@ void main() {
     vec4 color1 = texture2D(uImage1, uv);
     vec4 color2 = texture2D(uImage2, uv);
 
-    // float time = sin(uTime) + 1.0;
-    // time /= 2.0;
-    // time *= 1.5;
-    // time -= 0.5;
+    float st = uStrength;
 
-    float strength = smoothstep(uStrength + 0.0, uStrength + 0.5, vUv.x);
+    float strength = smoothstep(st + 0.5, st + 0.0, vUv.x);
 
     vec4 color = mix(color1, color2, strength);
 
